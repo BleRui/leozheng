172818b486c5ec98319cd8ccb2408a3060c8472a
LeoZheng <LeoZheng@canway.net>
2020-03-13 10:59:31 +0800

    leozhengtest

--- ui/src/api/index.js
+++
@@ -1,8 +0,0 @@
-// 统一引入api模块
-import Server from './apiUrl/server'
-import User from './apiUrl/user'
-
-export default {
-    User,
-    Server
-}
--- ui/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import Vuex from 'vuex'
-import Vue from 'vue'
-import product from './modules/product'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-    modules: {product}
-})
